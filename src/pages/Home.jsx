877b96f19b7896454c2068b3eea94a834d8f3e5c
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-06-11 22:00:36 -0300

    about me home container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,12 +14,17 @@ const Home = () => {
                         <h2>Kaique Melo</h2>
                         <p>Freelancer, Desenvolvedor & Estudante</p>
                         <ul>
-                            <li><a href=""><FaGithub /></a></li>
-                            <li><a href=""><FaLinkedin /></a></li>
-                            <li><a href=""><MdEmail /></a></li>
+                            <li><a href="https://github.com/KaiD3v"><FaGithub /></a></li>
+                            <li><a href="https://www.linkedin.com/in/kaique-ricardo-de-melo-98969b256/"><FaLinkedin /></a></li>
+                            <li><a href="mailto:kaikricardo99@gmail.com"><MdEmail /></a></li>
                         </ul>
                     </section>
                 </div>
+            </section>
+            <section className="about-container">
+                <h2>Olá!</h2>
+                <p>Meu nome é Kaique, e sou um desenvolvedor fullstack apaixonado por solucionar problemas através da programação.</p>
+                <p>No momento, trabalho como freelancer, onde utilizo as stacks <a href="https://nextjs.org/docs">Next.js</a> e <a href="https://docs.nestjs.com/">Nest.js</a> do JavaScript para criar soluções inovadoras e eficientes para meus clientes.</p>
             </section>
         </div>
     )
