84f3099ef3b0ebee27da19f86950cb723a0ccf64
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-06-11 21:22:07 -0300

    profile container styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const Home = () => {
-  return (
-    <div className='home-container'>
-        <section id='profile' className='profile-container'>
-
-        </section>
-    </div>
-  )
+    return (
+        <div className='home-container'>
+            <section id='profile' className='profile-container'>
+                <div>
+                    <section className="profile-picture">
+                        <img src="https://avatars.githubusercontent.com/u/126838751?v=4" alt="profile" />
+                    </section>
+                    <section className="profile-description">
+                        <h2>Kaique Melo</h2>
+                        <p>Freelancer, Desenvolvedor & Estudante</p>
+                        <ul>
+                            <li><a href=""><FaGithub /></a></li>
+                            <li><a href=""><FaLinkedin /></a></li>
+                            <li><a href=""><MdEmail /></a></li>
+                        </ul>
+                    </section>
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default Home
