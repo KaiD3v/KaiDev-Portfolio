3bb08277f1c187624c79cb938ae7a6d01ce44a2a
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-10-27 20:53:22 -0300

    refactore projects cards content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,6 +23,60 @@ import CardProject from "../components/CardProject";
 
 import emailjs from "@emailjs/browser";
 
+const worksCardsContents = [
+  {
+    id: 1,
+    projectLink: "/works/1",
+    projectName: "Gerenciador de Brechó",
+    projectImg: thriftStoreImg,
+    projectDesc: "Uma aplicação de gerenciamento para lojas e brechós.",
+    projectTechIcons: [<FaReact />, <SiExpress />, <RiTailwindCssFill />],
+  },
+  {
+    id: 2,
+    projectLink: "/works/2",
+    projectName: "Site de Documentação - O Mistério de IJatu",
+    projectImg: ijatuMysteryImg,
+    projectDesc: "Um site de documentação para um RPG de mesa.",
+    projectTechIcons: [<FaReact />, <SiTypescript />, <RiTailwindCssFill />],
+  },
+];
+
+const projectsCardsContents = [
+  {
+    id: 1,
+    projectLink: "/projects/4",
+    projectName: "Tasks+",
+    projectImg: tasksPlus,
+    projectDesc: "Um site para gerenciar tarefas",
+    projectTechIcons: [<RiNextjsFill />],
+  },
+  {
+    id: 2,
+    projectLink: "/projects/2",
+    projectName: "My Repos",
+    projectImg: myRepos,
+    projectDesc: "Um site para armazenar repositórios do Github",
+    projectTechIcons: [<RiNextjsFill />],
+  },
+  {
+    id: 3,
+    projectLink: "/projects/1",
+    projectName: "MiniBlog",
+    projectImg: miniblogImg,
+    projectDesc: "Um pequeno blog que permite realizar postagens",
+    projectTechIcons: [<FaReact />],
+  },
+  {
+    id: 4,
+    projectLink: "/projects/3",
+    projectName: "ReactGram",
+    projectImg: reactgramImg,
+    projectDesc: "Um clone do instagram feito com ReactJs",
+    projectTechIcons: [<FaReact />],
+  },
+];
+
 const Home = () => {
   const [projectContainer, setProjectContainer] = useState("projects");
   const [name, setName] = useState("");
@@ -156,28 +210,17 @@ const Home = () => {
                 viewport={{ once: true }}
                 className="home-container"
               >
-                <CardProject
-                  projectLink={"/works/1"}
-                  projectName="Gerenciador de Brechó"
-                  projectImg={thriftStoreImg}
-                  projectDesc="Uma aplicação de gerenciamento para lojas e brechós."
-                  projectTechIcons={[
-                    <FaReact />,
-                    <SiExpress />,
-                    <RiTailwindCssFill />,
-                  ]}
-                />
-                <CardProject
-                  projectLink={"/works/2"}
-                  projectName="Site de Documentação - O Mistério de IJatu"
-                  projectImg={ijatuMysteryImg}
-                  projectDesc="Um site de documentação para um RPG de mesa."
-                  projectTechIcons={[
-                    <FaReact />,
-                    <SiTypescript />,
-                    <RiTailwindCssFill />,
-                  ]}
-                />
+                {worksCardsContents.length > 0 &&
+                  worksCardsContents.map((content, index) => (
+                    <CardProject
+                      key={content.id}
+                      projectLink={content.projectLink}
+                      projectName={content.projectName}
+                      projectImg={content.projectImg}
+                      projectDesc={content.projectDesc}
+                      projectTechIcons={content.projectTechIcons || "..."}
+                    />
+                  ))}
               </m.main>
             )}
             {projectContainer === "projects" && (
@@ -187,34 +230,17 @@ const Home = () => {
                 viewport={{ once: true }}
                 className="home-container"
               >
-                <CardProject
-                  projectLink={"/projects/4"}
-                  projectName="Tasks+"
-                  projectImg={tasksPlus}
-                  projectDesc="Um site para armazenar repositórios do Github"
-                  projectTechIcons={<RiNextjsFill />}
-                />
-                <CardProject
-                  projectLink={"/projects/2"}
-                  projectName="My Repos"
-                  projectImg={myRepos}
-                  projectDesc="Um site para armazenar repositórios do Github"
-                  projectTechIcons={<FaReact />}
-                />
-                <CardProject
-                  projectLink={"/projects/1"}
-                  projectName="MiniBlog"
-                  projectImg={miniblogImg}
-                  projectDesc="Um pequeno blog que permite realizar postagens"
-                  projectTechIcons={<FaReact />}
-                />
-                <CardProject
-                  projectLink={"/projects/3"}
-                  projectName="ReactGram"
-                  projectImg={reactgramImg}
-                  projectDesc="Um clone do instagram feito com ReactJs"
-                  projectTechIcons={<FaReact />}
-                />
+                {projectsCardsContents.length > 0 &&
+                  projectsCardsContents.map((content, index) => (
+                    <CardProject
+                      key={content.id}
+                      projectLink={content.projectLink}
+                      projectName={content.projectName}
+                      projectImg={content.projectImg}
+                      projectDesc={content.projectDesc}
+                      projectTechIcons={content.projectTechIcons || "..."}
+                    />
+                  ))}
               </m.main>
             )}
           </section>
